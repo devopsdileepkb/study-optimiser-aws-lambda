89e80310b956f5231d9ab8736e3727c66cca99a1
Praveenjnj <Praveenjnj34@gmail.com>
2026-02-24 11:29:33 +0530

    Update provider.tf

--- iac/provider.tf
+++ iac/provider.tf
@@ -1,1 +1,16 @@
-hi
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+    region = var.region
+    
+  }
+
