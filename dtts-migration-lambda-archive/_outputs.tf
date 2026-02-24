1f99e0597aff1915555eaf763259e17862067349
devopsdileepkb <dilee8217@gmail.com>
2026-02-24 16:27:06 +0530

    Update _outputs.tf

--- dtts-migration-lambda-archive/_outputs.tf
+++ dtts-migration-lambda-archive/_outputs.tf
@@ -1,4 +1,4 @@
-output "lambda_role_arn" {
+/*output "lambda_role_arn" {
   value = module.iam_role.role_arn
 }
 
@@ -13,3 +13,4 @@ output "lambda_function_arn" {
 output "event_rule_arn" {
   value = module.lambda_trigger.event_rule_arn
 }
+*/
