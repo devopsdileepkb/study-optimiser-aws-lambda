974fd883ca726819e7dffe92aeecd90a751a7e9d
devopsdileepkb <dilee8217@gmail.com>
2026-02-24 13:56:58 +0530

    5

--- modules/lambda_functions/main.tf
+++ modules/lambda_functions/main.tf
@@ -1,57 +1,36 @@
 resource "aws_lambda_function" "zip_csv_lambda" {
-  /* Creates a Lambda function.
-  https://registry.terraform.io/providers/hashicorp/aws/5.97.0/docs/resources/lambda_function */
-  function_name                  = var.function_name
-  package_type                   = var.package_type
+  count = lower(var.package_type) == "zip" ? 1 : 0
 
-  #zip specific
+  function_name = var.function_name
+  package_type  = "Zip"
 
-  filename                       = var.package_type == "Zip" ? var.filename : null
-  source_code_hash = var.package_type == "Zip" && var.filename !=null ? filebase64sha256(var.filename) : null
-  handler = var.package_type == "Zip" ? var.handler : null
-  runtime = var.package_type == "Zip" ? var.runtime : null
-
-  #image specific
-  image_uri = var.package_type == "Image" ? var.image_url : null
+  filename         = var.filename
+  source_code_hash = filebase64sha256(var.filename)
+  handler          = var.handler
+  runtime          = var.runtime
 
   memory_size = var.memory_size
-  timeout = var.timeout
-  role                           = var.lambda_role_arn
+  timeout     = var.timeout
+  role        = var.lambda_role_arn
   architectures = [var.lambda_architecture]
   reserved_concurrent_executions = var.reserved_concurrent_executions
-  layers                         = var.layers_arns
-  kms_key_arn                    = var.kms_key_arn != null ? var.kms_key_arn : null
-  dynamic "vpc_config" {
-    for_each = var.enable_vpc ? [1] : []
-    content {
+  layers      = var.layers_arns
+  kms_key_arn = var.kms_key_arn
+}
 
-    subnet_ids         = var.subnet_ids
-    security_group_ids = var.security_group_ids
-  }
-    }
-  tracing_config { # Required for Compliance
-    mode = "Active"
-  }
-  dynamic "logging_config" {
-    for_each = length(var.cloudwatch_log_group_name) > 0 ? [1] : []
-    content {
-      log_format = "Text"
-      log_group  = var.cloudwatch_log_group_name
-    }
-  }
-  dynamic "logging_config" {
-    for_each = length(var.cloudwatch_log_group_name) == 0 ? [1] : []
-    content {
-      log_format = "Text"
-    }
-  }
-  dynamic "environment" {
-    for_each = length(var.environment_variables) > 0 ? [1] : []
-    content {
-      variables = var.environment_variables
-    }
-  }
+resource "aws_lambda_function" "image_lambda" {
+  count = lower(var.package_type) == "image" ? 1 : 0
 
-  }
+  function_name = var.function_name
+  package_type  = "Image"
 
+  image_uri = var.image_url
 
+  memory_size = var.memory_size
+  timeout     = var.timeout
+  role        = var.lambda_role_arn
+  architectures = [var.lambda_architecture]
+  reserved_concurrent_executions = var.reserved_concurrent_executions
+  layers      = var.layers_arns
+  kms_key_arn = var.kms_key_arn
+}
